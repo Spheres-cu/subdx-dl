8300311bd4697021d831ab692b38369a0a2f5f04
Spheres_cu <72468068+Spheres-cu@users.noreply.github.com>
2025-08-14 10:33:41 -0400

    Fixed timeout error
    - Increased: retries, read time and timeout to avoid the slow server response.

--- sdx_dl/sdxutils.py
+++ sdx_dl/sdxutils.py
@@ -84,9 +84,9 @@ if args.proxy:
     proxie = f"{args.proxy}"
     if not (any(p in proxie for p in ["http", "https"])):
         proxie = "http://" + proxie
-    conn = urllib3.ProxyManager(proxie, headers=headers, cert_reqs="CERT_REQUIRED", ca_certs=certifi.where(),  retries=2, timeout=15)
+    conn = urllib3.ProxyManager(proxie, headers=headers, cert_reqs="CERT_REQUIRED", ca_certs=certifi.where(),  retries=3, timeout=40)
 else:
-    conn = urllib3.PoolManager(headers=headers, cert_reqs="CERT_REQUIRED", ca_certs=certifi.where(), retries=2, timeout=15)
+    conn = urllib3.PoolManager(headers=headers, cert_reqs="CERT_REQUIRED", ca_certs=certifi.where(), retries=3, timeout=40)
 
 # Network connections Errors
 def HTTPErrorsMessageException(e: HTTPError) -> None:
@@ -476,7 +476,7 @@ def get_aadata(search:str) -> Any:
     """Get a json data with the ``search`` results."""
     json_aaData:Any = ''
     try:
-        retries = urllib3.util.Retry(total=2, backoff_factor=1)
+        retries = urllib3.util.Retry(total=3, read=10, backoff_factor=1)
         fields:Dict[str, Any] = {'buscar' + conn_data.search: search,
         'filtros': '', 'tabla': 'resultados', 'token': conn_data.token
         }
